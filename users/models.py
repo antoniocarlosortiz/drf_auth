863c14021dc4d56cc6428a36ceef3bd5647d2233
Antonio Carlos L. Ortiz <ortizantoniocarlos@gmail.com>
2016-12-19 00:54:39 +0800

    setup required User model

--- users/models.py
+++ users/models.py
@@ -1,5 +1,54 @@
 from __future__ import unicode_literals
 
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from django.core.mail import send_mail
 from django.db import models
+from django.utils import timezone
+
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, **kwargs):
+        email = self.normalize_email(email)
+        user = self.model(email=email, **kwargs)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, **kwargs):
+        user = self.create_user(**kwargs)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self._db)
+        return user
+
 
 # Create your models here.
+class User(AbstractBaseUser):
+    email = models.EmailField('email address', max_length=255, unique=True)
+    first_name = models.CharField('first name', max_length=255, blank=True)
+    last_name = models.CharField('last name', max_length=255, blank=True)
+    is_staff = models.BooleanField('staff status', default=False,
+        help_text='Designates whether the user can log into this admin site.')
+    is_active = models.BooleanField('active', default=True,
+        help_text=('Designates whether this user should be treated as '
+                  'active. Unselect this instead of deleting accounts.'))
+    date_joined = models.DateTimeField('date joined', default=timezone.now)
+
+
+    USERNAME_FIELD = 'email'
+    objects = UserManager()
+
+    class Meta:
+        ordering = ['email']
+
+    def get_full_name(self):
+        return self.email
+
+    def get_short_name(self):
+        return self.email
+
+    def email_user(self, subject, message, from_email=None, **kwargs):
+        """
+        Sends an email to this User.
+        """
+        send_mail(subject, message, from_email, [self.email], **kwargs)
