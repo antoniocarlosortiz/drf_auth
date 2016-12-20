7c2b2d2a7a9cad2d266448081ca1b5fbea2120ca
Antonio Carlos L. Ortiz <ortizantoniocarlos@gmail.com>
2016-12-20 14:16:22 +0800

    fix bug: REST_FRAMEWORK setting overlapping

--- drf_auth/settings.py
+++ drf_auth/settings.py
@@ -132,9 +132,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
-    )
-}
-
-REST_FRAMEWORK = {
-    'PAGE_SIZE': 10
+    ),
+    'PAGE_SIZE': 10,
 }
