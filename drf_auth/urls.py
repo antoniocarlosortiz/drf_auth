0716c60e7087d4271d2a7cbae44e4d45f4485787
Antonio Carlos L. Ortiz <ortizantoniocarlos@gmail.com>
2016-12-20 12:51:07 +0800

    add namespace

--- drf_auth/urls.py
+++ drf_auth/urls.py
@@ -18,5 +18,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^', include('users.urls')),
+    url(r'^users/', include('users.urls', namespace='users')),
 ]
