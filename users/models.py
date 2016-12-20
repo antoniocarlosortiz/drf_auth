38462d5cf36a97758682036797366e26e31039ca
Antonio Carlos L. Ortiz <ortizantoniocarlos@gmail.com>
2016-12-20 14:43:40 +0800

    pep8

--- users/models.py
+++ users/models.py
@@ -32,11 +32,13 @@ class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField('email address', max_length=255, unique=True)
     first_name = models.CharField('first name', max_length=255, blank=True)
     last_name = models.CharField('last name', max_length=255, blank=True)
-    is_staff = models.BooleanField('staff status', default=False,
+    is_staff = models.BooleanField(
+        'staff status', default=False,
         help_text='Designates whether the user can log into this admin site.')
-    is_active = models.BooleanField('active', default=False,
+    is_active = models.BooleanField(
+        'active', default=False,
         help_text=('Designates whether this user should be treated as '
-                  'active. Unselect this instead of deleting accounts.'))
+                   'active. Unselect this instead of deleting accounts.'))
     date_joined = models.DateTimeField('date joined', default=timezone.now)
     confirm_key = models.CharField(max_length=40, unique=True)
 
